0b3b4f4fd1cf7632c717c9b5c413328dd6b51ede
Ke+Natasha <Ke+Natasha@appacademy.io>
2013-05-15 09:09:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewspaperApp::Application.config.secret_token = '1392f742e06315245a6e33526edb3373a01275029fd91946c717b4559b9927cea813344a1a53fd6672be7e393b5fb0bf86d58fd32de431ef7528c0861368666c'
